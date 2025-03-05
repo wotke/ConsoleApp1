fa7cabc47b3248b8fcd50a83c437b2c59bd8215a
TOMMY ZIELINSKI <tommy.zielinski@teb.edu.pl>
2025-03-05 13:28:27 +0100

    Dodano obsługę różnych operacji matematycznych
    Zmieniono kod, aby dodać obsługę operacji matematycznych (+, -, *, /) zamiast tylko dodawania dwóch liczb. Usunięto linię `double result = a + b;`. Dodano komunikat do użytkownika: "Wybierz rodzaj dzialania: ". Dodano odczytanie operacji matematycznej od użytkownika. Dodano instrukcję `switch` do obsługi różnych operacji matematycznych (dodawanie, odejmowanie, mnożenie, dzielenie). Zmieniono sposób wyświetlania wyniku, aby uwzględniał wybraną operację.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,5 +8,27 @@ double a = double.Parse(buffer);
 Console.WriteLine("Podaj druga liczbe");
 buffer = Console.ReadLine() ?? "0";
 double b = double.Parse(buffer);
-double result = a + b;
+
+Console.WriteLine("Wybierz rodzaj dzialania: ");
+buffer = Console.ReadLine() ?? "0";
+char operation = buffer[0];
+    double result;
+switch(operation)
+{
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    default:
+        result = 0;
+        break;
+}
 Console.WriteLine("Wynik dodawania liczb " + a + " i " + b + " wynosi: " + result);
