448b44dcc57e254d558e5bb095ca52850cb901c7
4625385 <4625385@TE-GDA-D-D21E15>
2025-03-05 13:09:44 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Podaj pierwsza liczbe");
+string buffer = Console.ReadLine();
+double a = double.Parse(buffer);
+
+Console.WriteLine("Podaj druga liczbe");
+buffer = Console.ReadLine() ?? "0";
+double b = double.Parse(buffer);
+double result = a + b;
+Console.WriteLine("Wynik dodawania liczb " + a + " i " + b + " wynosi: " + result);
